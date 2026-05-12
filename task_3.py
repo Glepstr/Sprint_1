e0c8f833a64bd1bbbe6ee4a27287c53d46c49fd6
Glep Glepovski <pautov.glebster@yandex.ru>
2026-05-12 11:02:36 +0300

    Сделал построчный вывод словаря

--- task_3.py
+++ task_3.py
@@ -7,7 +7,8 @@ world_champions = {
 }
 
 world_champions['2022'] = 'Аргентина'
-print(world_champions)
+for key, value in world_champions.items():
+    print(f"{key}: {value}")
 
 country = 'Италия'
 
